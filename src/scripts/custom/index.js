d31fd1d4715450842363b56c4600f2ec994d2279
Thai Son Dang <thaisondang.bk57@gmail.com>
2018-12-14 03:07:19 +0100

    toggle SideDrawer funnction

--- src/scripts/custom/index.js
+++ src/scripts/custom/index.js
@@ -1,1 +1,25 @@
-document.addEventListener("DOMContentLoaded", function(event) {})
+const state = {
+  showSideDrawer: false
+}
+
+window.onload = e => {
+  const screenHeight = window.screen.availHeight
+  const screenWidth = window.screen.availWidth
+
+  const toggleSideDrawer = () => {
+    !state.showSideDrawer
+      ? (document.querySelector("#Nav").style.transform = "translateX(0)")
+      : (document.querySelector("#Nav").style.transform = "translateX(-110%)")
+
+    document.querySelector("#nav-toggle").classList.toggle("active")
+
+    // toggle showSideDrawer property
+    state.showSideDrawer = !state.showSideDrawer
+  }
+
+  if (screenWidth <= 900) {
+    document.querySelector("#NavToggle").addEventListener("click", e => {
+      toggleSideDrawer()
+    })
+  }
+}
