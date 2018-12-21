3b827f110bfd656a1ca7cb24d0d489e04237c564
Thai Son Dang <thaisondang.bk57@gmail.com>
2018-12-21 13:53:27 +0100

    added lazyload for images

--- src/client/scripts/custom/index.js
+++ src/client/scripts/custom/index.js
@@ -1,27 +1,51 @@
 const state = {
   showSideDrawer: false
-}
-const screenHeight = window.screen.availHeight
-const screenWidth = window.screen.availWidth
+};
+const screenHeight = window.screen.availHeight;
+const screenWidth = window.screen.availWidth;
 
 $(function() {
   if (screenWidth <= 900) {
-    toggleNavBar()
+    toggleNavBar();
   }
-})
+});
 
 function toggleNavBar() {
   const toggleSideDrawer = () => {
     !state.showSideDrawer
       ? (document.querySelector("#Nav").style.transform = "translateX(0)")
-      : (document.querySelector("#Nav").style.transform = "translateX(-110%)")
+      : (document.querySelector("#Nav").style.transform = "translateX(-110%)");
 
-    document.querySelector("#nav-toggle").classList.toggle("active")
+    document.querySelector("#nav-toggle").classList.toggle("active");
 
     // toggle showSideDrawer property
-    state.showSideDrawer = !state.showSideDrawer
-  }
+    state.showSideDrawer = !state.showSideDrawer;
+  };
   document.querySelector("#NavToggle").addEventListener("click", e => {
-    toggleSideDrawer()
-  })
+    toggleSideDrawer();
+  });
 }
+
+function lazyImage() {
+  const images = Array.prototype.slice.call(
+    document.getElementsByClassName("lazy")
+  );
+
+  images.forEach(image => {
+    if (image.getBoundingClientRect().top < window.innerHeight - 100) {
+      const src = image.dataset.src;
+      image.setAttribute("src", src);
+      image.classList.add("show");
+    }
+
+    window.addEventListener("scroll", () => {
+      if (image.getBoundingClientRect().top < window.innerHeight - 100) {
+        const src = image.dataset.src;
+        image.setAttribute("src", src);
+        image.classList.add("show");
+      }
+    });
+  });
+}
+
+lazyImage();
